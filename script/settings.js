0826442f5b793087e242e5d89e70dd25c1f445f6
flarom <151688146+flarom@users.noreply.github.com>
2025-06-10 21:28:51 -0300

    added more jsdoc

--- script/settings.js
+++ script/settings.js
@@ -1,9 +1,21 @@
 const settingsStorageKey = "COHESION_SETTINGS_";
 
+/**
+ * Sets a new value to a setting
+ * @param {string} key Setting name
+ * @param {string} value Setting value
+ */
 function setSetting(key, value) {
     localStorage.setItem(settingsStorageKey + key, value);
 }
 
+/**
+ * Gets the value of a setting
+ * @param {string} key Setting name
+ * @param {any} fallback Returned value in case of the setting not being found
+ * @param {boolean} ignoreComments Ignore lines starting with '#'
+ * @returns The setting value as string
+ */
 function getSetting(key, fallback = null, ignoreComments = false) {
     let value = localStorage.getItem(settingsStorageKey + key);
     if (!value) return fallback;
@@ -15,10 +27,17 @@ function getSetting(key, fallback = null, ignoreComments = false) {
     return value;
 }
 
+/**
+ * Deletes the value of a setting
+ * @param {string} key Setting name
+ */
 function removeSetting(key) {
     localStorage.removeItem(settingsStorageKey + key);
 }
 
+/**
+ * Deletes all settings
+ */
 function clearAllSettings() {
     for (let i = 0; i < localStorage.length; i++) {
         const key = localStorage.key(i);
