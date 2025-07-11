cdfdce284df77944a70062cfa910d58fbd441442
flarom <151688146+flarom@users.noreply.github.com>
2025-07-11 20:58:25 -0300

    commands now use fuzzy search

--- script/codemirror/addon/hint/command-hint.js
+++ script/codemirror/addon/hint/command-hint.js
@@ -34,7 +34,7 @@
                 insertBlock(items);
             }
         },
-        "ul": {
+        "unorderedlist": {
             description: "Unordered list",
             exec: function(arg) {
                 let count = parseInt(arg, 10) || 3;
@@ -42,7 +42,7 @@
                 insertBlock(items);
             }
         },
-        "ol": {
+        "orderedlist": {
             description: "Ordered list",
             exec: function(arg) {
                 let count = parseInt(arg, 10) || 3;
@@ -71,7 +71,45 @@
                 insertBlock(getTable(count, 2));
             }
         },
+        "image": {
+            description: "Insert an image",
+            exec: function(arg) {
+                insertAt("![ALT TEXT](URL)", 12, 15);
+            }
+        },
+        "url": {
+            description: "Insert an URL",
+            exec: function(arg) {
+                insertAt("[TITLE](URL)", 8, 11);
+            }
+        },
+        "link": {
+            description: "Insert an URL",
+            exec: function(arg) {
+                insertAt("[TITLE](URL)", 8, 11);
+            }
+        },
+        "anchor": {
+            description: "Insert an URL",
+            exec: function(arg) {
+                insertAt("[TITLE](URL)", 8, 11);
+            }
+        }
     };
+
+    function fuzzyMatch(str, pattern) {
+        pattern = pattern.replace(/:/g, "");
+        if (!pattern) return true;
+        let patternIdx = 0,
+            strIdx = 0;
+        while (strIdx < str.length && patternIdx < pattern.length) {
+            if (str[strIdx].toLowerCase() === pattern[patternIdx].toLowerCase()) {
+                patternIdx++;
+            }
+            strIdx++;
+        }
+        return patternIdx === pattern.length;
+    }
 
     CodeMirror.registerHelper("hint", "command", function (editor, options) {
         var cur = editor.getCursor(),
@@ -83,44 +121,27 @@
         var curWord = curLine.slice(start, end);
 
         var list = [];
-        if (!curWord) {
-            for (var key in COMMANDS) {
+        var match = curWord.match(/^([a-zA-Z]+)?(\d*)$/);
+        var cmd = match?.[1] || "";
+        var arg = match?.[2] || "";
+
+        for (var key in COMMANDS) {
+            if (!cmd || fuzzyMatch(key, cmd)) {
                 (function(commandKey) {
                     list.push({
-                        text: "/" + commandKey,
+                        text: "/" + commandKey + (arg ? arg : ""),
                         displayText: "/" + commandKey + " - " + COMMANDS[commandKey].description,
                         hint: function(cm, data, completion) {
                             var from = CodeMirror.Pos(cur.line, start - 1);
                             var to = CodeMirror.Pos(cur.line, end);
                             cm.replaceRange("", from, to);
-                            COMMANDS[commandKey].exec();
+                            COMMANDS[commandKey].exec(arg);
                         }
                     });
                 })(key);
             }
-        } else {
-            var match = curWord.match(/^([a-zA-Z]+)(\d*)$/);
-            if (!match) return;
-            var cmd = match[1];
-            var arg = match[2];
-
-            for (var key in COMMANDS) {
-                if (key.indexOf(cmd) === 0) {
-                    (function(commandKey) {
-                        list.push({
-                            text: "/" + commandKey + (arg ? arg : ""),
-                            displayText: "/" + commandKey + " - " + COMMANDS[commandKey].description,
-                            hint: function(cm, data, completion) {
-                                var from = CodeMirror.Pos(cur.line, start - 1);
-                                var to = CodeMirror.Pos(cur.line, end);
-                                cm.replaceRange("", from, to);
-                                COMMANDS[commandKey].exec(arg);
-                            }
-                        });
-                    })(key);
-                }
-            }
         }
+
         return {
             list: list,
             from: CodeMirror.Pos(cur.line, start - 1),
