5f2fdb297073e65343adb21c73a04c722c8006e6
flarom <flarocontato@gmail.com>
2025-12-23 14:56:56 -0300

    disabled broken figcaptions

--- script/showdown/extension/figcaption.js
+++ script/showdown/extension/figcaption.js
@@ -1,40 +1,57 @@
-showdown.extension("figureCaption", function() {
+showdown.extension("figureCaption", function () {
 
     function renderInlineMD(md) {
+        if (!md) return "";
         const conv = new showdown.Converter();
         const html = conv.makeHtml(md.trim());
-        return html.replace(/^<p>|<\/p>$/g, '');
+        return html.replace(/^<p>|<\/p>$/g, "");
     }
 
     return [
         {
-            type: 'output',
+            type: "output",
 
-            regex: /<a\s+href="([^"]+)"[^>]*>\s*([\s\S]*?)<img([^>]*?)alt="([^"]*?)"([^>]*?)title="([^"]*?)"([^>]*?)>([\s\S]*?)<\/a>/g,
+            regex: /<a\s+href="([^"]+)"[^>]*>\s*<img([^>]*?)alt="([^"]*?)"([^>]*?)title="([^"]*?)"([^>]*?)>\s*<\/a>/g,
 
-            replace: function(match, url, beforeImg, beforeAlt, alt, between, title, afterAlt) {
+            replace: function (match, url, beforeAlt, alt, between, title) {
 
                 const srcMatch = match.match(/src="([^"]*?)"/);
                 const src = srcMatch ? srcMatch[1] : "";
 
                 const caption = renderInlineMD(title);
 
-                return `<figure><img src="${src}" alt="${alt}" onclick="window.open('${url}')" style="cursor:pointer;"><figcaption>${caption}<div style='text-align:right'><a href='${url}' target='_blank'>${url}</a></div></figcaption></figure>`;
+                return (
+                    `<figure>` +
+                        `<img src="${src}" alt="${alt}" ` +
+                        `onclick="window.open('${url}')" style="cursor:pointer;">` +
+                        `<figcaption>` +
+                            caption +
+                            `<div style="text-align:right">` +
+                                `<a href="${url}" target="_blank">${url}</a>` +
+                            `</div>` +
+                        `</figcaption>` +
+                    `</figure>`
+                );
             }
         },
-
         {
-            type: 'output',
-            regex: /<img([^>]*?)alt="([^"]*?)"([^>]*?)title="([^"]*?)"([^>]*?)>/g,
-            replace: function(match) {
+            type: "output",
 
-                const alt = match.match(/alt="([^"]*?)"/)[1];
-                const src = match.match(/src="([^"]*?)"/)[1];
-                const title = match.match(/title="([^"]*?)"/)[1];
+            regex: /<img([^>]*?)alt="([^"]*?)"([^>]*?)title="([^"]*?)"([^>]*?)>/g,
+
+            replace: function (match, beforeAlt, alt, between, title) {
+
+                const srcMatch = match.match(/src="([^"]*?)"/);
+                const src = srcMatch ? srcMatch[1] : "";
 
                 const caption = renderInlineMD(title);
 
-                return `<figure><img src="${src}" alt="${alt}"><figcaption>${caption}</figcaption></figure>`;
+                return (
+                    `<figure>` +
+                        `<img src="${src}" alt="${alt}">` +
+                        `<figcaption>${caption}</figcaption>` +
+                    `</figure>`
+                );
             }
         }
     ];
